90be49b48275bbe111084b591971270862d7f3c3
Farrah Carter <carterfarraha@gmail.com>
2019-10-25 13:33:41 -0400

    Search and limit are working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
+    let search = "election";
+    let limit = 3;
+    let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +search  + "&api-key=4T4JAn6PPSJW7c7RpRNUgAK4qSQQxGio";
 
+    $.ajax ({
+        url: queryURL,
+        method: "GET"
+    }).then (function(response){
+
+        for (let i =0; i < limit; i++) {
+        
+        console.log(response.response.docs[i]);
+
+        }
+
+
+    })
 
 
 
